074d3ac5540644d301e22897b4d7f466f603911f
Paul Kopetko <paulkopetko@gmail.com>
2014-01-24 22:42:40 +1100

    Enhanced audio engine with Adam's help (now "currently playing sounds" are accessable to new events and variables)

--- js/GJ.Sound.js
+++ js/GJ.Sound.js
@@ -2,39 +2,61 @@ GJ.Sound = (function () {
 	'use strict';
 
 	// Private variables //////////////////////////////////////////
-	var sfx = {
+	var	soundsPlaying = [];
 
-	};
 
 	// Private methods ////////////////////////////////////////////
 
-	var init = function ( ) {
-		
-		// Start music loops
-		var music_background = createjs.Sound.play("placeholder_02", {loop: 10});
-		music_background.volume = 0;
-		fadeVolume(music_background, 1, 2000);
-	},
-
 	// Generic fading function
-	fadeVolume = function (soundInstance, targetVolume, fadeDuration) {
+	var fadeVolume = function (soundInstance, targetVolume, fadeDuration) {
 		createjs.Tween.get(soundInstance).to({volume:targetVolume}, fadeDuration);
 	}
-
-
 
 	// Public methods /////////////////////////////////////////////
 
 	return {
 
-		init: init,
-		playFootstep: function ( ) {
-			var randomInt = Math.floor((Math.random()*3)+1);
-			var footstep = createjs.Sound.play("placeholder_0" + randomInt, {loop: 50});
+		init: function ( ) {
+		
+			// Start music loops
+			soundsPlaying.push({
+				name: "BGM",
+				sound: createjs.Sound.play("placeholder_02", {loop: 50})
+			});
+			music_background.volume = 0;
+			fadeVolume(music_background, 1, 2000);
 		},
 
-		changeVolume: function ( ) {
-			playFootstep.footstep.volume = 0.5;
+		playFootstep: function ( ) { // model for playing random sounds
+			var randomInt = Math.floor((Math.random()*3)+1);
+			soundsPlaying.push({
+				name: "footstep",
+				sound: createjs.Sound.play("placeholder_0" + randomInt, {loop: 50})
+			});
+		},
+
+		update: function ( ) {
+			var i, sound;
+
+			for (i = 0; i < soundsPlaying.length; i++) {
+				sound = soundsPlaying[i];
+
+				if (sound.name === "BGM") {
+					sound.sound.volume = 0.2;
+				} else if (sound.name === "explosion") {
+					
+				}
+				
+			}
+		},
+
+		triggerEvent: function (name) {
+			if (name === "explosion") {
+				soundsPlaying.push({
+					name: "explosion",
+					sound: createjs.Sound.play("placeholder_01", {loop: 10})
+				});
+			}
 		}
 
 	};
